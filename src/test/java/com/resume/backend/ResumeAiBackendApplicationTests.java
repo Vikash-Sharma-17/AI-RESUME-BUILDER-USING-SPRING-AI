eb0f47dbfd75af0dbe5555847cf27973ba579f9f
Sharma Vikash Kaulesh <94862026+Vikash-Sharma-17@users.noreply.github.com>
2025-06-02 12:52:54 +0530

    Add files via upload

---
+++ src/test/java/com/resume/backend/ResumeAiBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.resume.backend;
+
+import com.resume.backend.service.ResumeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+class ResumeAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
